1c6faea8eee1aafcd26351a95052af6a68f50aa4
Anders Andersen <anders.c.andersen@gmail.com>
2016-02-01 21:57:12 +0100

    Added comments on genetic algorithm

--- src/MetaHeuristics/GeneticAlgortihm/IGeneticAlgorithm.java
+++ src/MetaHeuristics/GeneticAlgortihm/IGeneticAlgorithm.java
@@ -1,10 +1,16 @@
 package MetaHeuristics.GeneticAlgortihm;
 
 public interface IGeneticAlgorithm {
+	// Generate initial population.
 	public void CreateInitialPopulation();
+	// Evaluate fitness level of population.
 	public void EvaluatePopulation();
+	// Returns true if a new child needs to be formed.
 	public boolean FormChildren();
+	// Form a new child from population
 	public void FormChild();
+	// Update current population with new children and kill least fit individuals
 	public void UpdatePopulation();
+	// Stopping criteria.
 	public boolean Stop();
 }
